20aa3d365eefdfee0a43e14c400e3b8ea536bc1b
PeterKao <peter0121@gmail.com>
2024-10-14 12:28:52 +0800

    Add ApiOrderController

---
+++ northwind_net8_api01/Controllers/ApiOrderController.cs
@@ -0,0 +1,76 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore.SqlServer.Storage.Internal;
+using northwind_net8_api01.DAL;
+using northwind_net8_api01.Models;
+using Polly;
+using System.Diagnostics;
+
+namespace northwind_net8_api01.Controllers
+{
+    [ApiController]
+    [Route("v1/order")]
+    public class ApiOrderController : ControllerBase
+    {
+        private readonly ILogger<ApiVer1Controller> _logger;
+        private INorthwind _northwind;
+        private Stopwatch _sw = new Stopwatch();
+        private Dictionary<string, string> _result = new Dictionary<string, string>();
+
+        public ApiOrderController(ILogger<ApiVer1Controller> logger, INorthwind northwind)
+        {
+            _logger = logger;
+            _northwind = northwind;
+            _sw.Start();
+        }
+
+        [HttpGet]
+        public IActionResult GetOrdersDefault()
+        {
+            return GetOrders(1, 10);
+        }
+
+        [HttpGet("{pageNumber}/{pageSize}")]
+        public IActionResult GetOrders(int pageNumber, int pageSize)
+        {
+            var SourceIP = HttpContext.Connection.RemoteIpAddress;
+
+            if (pageNumber <= 0 || pageSize <= 0)
+            {
+                return BadRequest("{}");
+            }
+
+            try
+            {
+                PagiOrderListModel result = null;
+                Policy
+                .Handle<SqlException>(
+                    ex => SqlServerTransientExceptionDetector.ShouldRetryOn(ex))
+                .Or<TimeoutException>()
+                .WaitAndRetry(2, retryAttempt => {
+                    // Adjust the retry interval as you see fit
+                    Random jitterer = new Random();
+                    return TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)) + TimeSpan.FromMilliseconds(jitterer.Next(0, 1000));
+                    }
+                )
+                .Execute(() =>
+                {
+                    result = _northwind.GetOrderList(pageNumber, pageSize);
+                });
+
+                if (result == null)
+                {
+                    return BadRequest("{}");
+                }
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"GetOrders, Error,'{SourceIP}' ,{ex.ToString()}");
+
+                return BadRequest("{}");
+            }
+        }
+    }
+}
